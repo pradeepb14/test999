3f7f35865fc32631e2973b4c63068a8641fa01b4
root <root@ip-172-31-18-156.ap-southeast-1.compute.internal>
2019-07-02 04:25:09 +0000

    test

---
+++ Test Suites/TestforLogin.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestforLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0f59c4a6-ac7f-4d09-9b55-8cbaec003927</testSuiteGuid>
+</TestSuiteEntity>
